113e1b891e42d47a6ed0dc880db538c56939163a
ATTALA VENKATA REDDY <36073543+venkat123reddy@users.noreply.github.com>
2024-11-23 15:52:35 -0600

    aws-configurations

---
+++ book-management-service/src/main/java/com/library/management_service/ManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.library.management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementServiceApplication.class, args);
+	}
+
+}
---
+++ book-management-service/src/test/java/com/library/management_service/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
